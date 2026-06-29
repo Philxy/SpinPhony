be49d37e68f7b76971745998e51ddc0954f99c85
Philxy <philxyphilxy@gmail.com>
2026-06-29 13:21:55 +0200

    band

--- python/SpinPhony.py
+++ python/SpinPhony.py
@@ -637,7 +637,6 @@ class CrystalDataSoA:
 
 
     def plot_hybridized_path_dispersions(self, filename="hybridized_path.png"):
-        import matplotlib.pyplot as plt
         
         # 1. Calculate piecewise k-distances
         k_distances = np.zeros(self.N_path)
