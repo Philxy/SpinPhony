c1f65ca977497f02603650ca09b6168b8d9310a5
Philxy <philxyphilxy@gmail.com>
2026-07-06 16:27:02 +0200

    band

--- python/SpinPhony.py
+++ python/SpinPhony.py
@@ -1934,7 +1934,7 @@ if __name__ == "__main__":
     slc_files = slc_files_bccFe
     band = band_bccFe
 
-    smearing = 1.0
+    smearing = 0.05
     
     crystal_data = CrystalDataSoA(
         mesh, 
