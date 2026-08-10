f84c2f72156021eaec2d48bff8f7be73be8d710a
Philxy <philxyphilxy@gmail.com>
2026-08-10 19:56:22 +0200

    whole BZ removed scaling

--- python/SpinPhony.py
+++ python/SpinPhony.py
@@ -751,12 +751,12 @@ class CrystalDataSoA:
                     val_n = -Omega_k[m, n]
                     if m == n:
                         val_n += anisotropy_term
-                    H_BdG[off_mag_p + m, off_mag_p + n] = val_n *  1.18 # artificial factor to match lit
+                    H_BdG[off_mag_p + m, off_mag_p + n] = val_n #*  1.18 # artificial factor to match lit for bccFe
 
                     val_h = -Omega_m_k[n, m]
                     if m == n:
                         val_h += anisotropy_term
-                    H_BdG[off_mag_h + m, off_mag_h + n] = val_h *  1.18 # artificial factor to match lit
+                    H_BdG[off_mag_h + m, off_mag_h + n] = val_h #*  1.18 # artificial factor to match lit for bccFe
 
             # --- SLC Interaction Blocks ---
             if hasattr(self, 'slc_axis') and is_FM:
